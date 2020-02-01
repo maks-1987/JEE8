84c403cbb2366c80e70a65277ccaf9ba4b6d65eb
MaxD <max.moderation@gmail.com>
2020-02-01 22:02:18 +0200

    reload package

--- src/main/java/Servlets/MathsServlet.java
+++ src/main/java/Servlets/MathsServlet.java
@@ -17,7 +17,6 @@ public class MathsServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
 
